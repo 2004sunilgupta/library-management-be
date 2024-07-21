bc564c896a935fc4712d2e6f6e06b49294cd526a
2004sunilgupta <2004sunilgupta@gmail.com>
2024-07-22 01:33:20 +0530

    DB Connection test

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express  = require("express");
 const dotenv = require("dotenv").config();
 const mongoose = require('mongoose');
 
+const dbURI = process.env.DB_CONNECTION;
+
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use('/api/books', require('./routes/booksRoutes'));
 
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err));
+
 
 app.listen(port, () => {
     console.log(`Server is runnig on port  ${port}`);
-    mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => {
-        console.log('connected db')
-    });
 });
