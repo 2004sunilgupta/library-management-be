34053f82ce09ec5518507eada636e23913568895
Sunil Gupta <58802886+2004sunilgupta@users.noreply.github.com>
2024-08-09 11:30:19 +0000

    books endpoint is in progress

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,11 +1,29 @@
+const Books = require('../models/bookModel');
+
 // @desc Get All Books
 // @route GET /api/books
 // access public
-
+exports.getAllBooks = async (req, res) => {
+    try {
+        const books  = await Books.find();
+        res.json(books);
+    } catch (err) {
+        res.status(500).send(err);
+    } 
+};
 
 // @desc Get Book
 // @route GET /api/books/:id
 // access public
+exports.createBook = async (req, res) => {
+    try {
+        const newBook = new Books(req.body);
+        const savedBook = await newBook.save();
+        res.status(201).send(savedBook);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
 
 // @desc Create New Book Entry
 // @route POST /api/books/:id
---
+++ src/models/bookModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    book_id: {type: String, required: true }
+});
+
+module.exports = mongoose.model('Books', bookSchema);
