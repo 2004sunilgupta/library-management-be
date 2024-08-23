8beb2ba6aed20513b4fbcf788c76e36274170ae8
Sunil Gupta <58802886+2004sunilgupta@users.noreply.github.com>
2024-08-23 22:13:00 +0000

    updated for vercel deployment

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
-const app = require('./app');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require('cors');
+const app = express();
 require("dotenv").config();
+const mongoose = require('mongoose');
 
 const dbURI = process.env.DB_URL;
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+const userRoutes = require('./routes/userRoutes');
+const bookRoutes = require('./routes/bookRoutes');
+const bookOperationsRoutes = require('./routes/bookOperationsRoutes');
+app.use('/api/users', userRoutes);
+app.use('/api/books', bookRoutes);
+app.use('/api/book-operations', bookOperationsRoutes);
 
 // Database connection
 mongoose.connect(dbURI)
