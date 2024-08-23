47e934c4b1719eb95f2c9ae7ac58e24f94362b7b
Sunil Gupta <58802886+2004sunilgupta@users.noreply.github.com>
2024-08-23 21:32:59 +0000

    vercel deployment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 
+// Configure CORS options
+// const corsOptions = {
+//     origin: 'https://orange-winner-4vrjjrg6p4g3j6xx-3000.app.github.dev', // Replace with your React app URL
+//     methods: 'GET,POST,PUT,DELETE', // Allowed methods
+//     credentials: true, // Enable cookies if needed
+// };
+
 // Middleware setup
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
