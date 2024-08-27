c6fed0ff410922c748186f5ebfc9aeae53ba1eba
Sunil Gupta <58802886+2004sunilgupta@users.noreply.github.com>
2024-08-27 05:49:39 +0000

    Book Model updated to update image url

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -4,6 +4,7 @@ const bookSchema = new mongoose.Schema({
     title: { type: String, required: true },
     bookId: { type: String, required: true, unique: true },
     author: { type: String, required: true },
+    imgURL: { type: String },
     genre: { type: String },
     publicationYear: { type: Number },
     available: { type: Boolean, default: true },
